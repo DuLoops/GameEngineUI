1d1991920fc8b787b04a32f79d1aa1e046003902
orion13579 <sloren@my.bcit.ca>
2022-03-18 11:36:37 -0700

    Added physics files

---
+++ FirstAttempt/CameraAndDynamicIndexing/PhysicsObject.cpp
@@ -0,0 +1,37 @@
+#include "PhysicsObject.h"
+#include <ppl.h>
+#include <algorithm>
+#include <vector>
+#include <cassert>
+
+using namespace DirectX;
+
+PhysicsObject::PhysicsObject(DirectX::XMFLOAT3 startingPosition, DirectX::XMFLOAT3 startingVelocity, DirectX::XMFLOAT3 startingForce, float startingMass, float dt)
+{
+	position = startingPosition;
+	velocity = startingVelocity;
+	force = startingForce;
+	mass = startingMass;
+	timeStep = dt;
+}
+
+PhysicsObject::~PhysicsObject()
+{
+}
+
+void PhysicsObject::Update(float dt)
+{
+	static float t = 0;
+
+	// Accumulate time.
+	t += dt;
+
+	// Only update the simulation at the specified time step.
+	if (t >= timeStep)
+	{
+		XMMATRIX velocityTranslationMatrix = XMMatrixTranslation(velocity.x * t, velocity.y * t, velocity.z * t);
+		position = position * velocityTranslationMatrix;
+		t = 0.0f; // reset time
+	}
+}
+
---
+++ FirstAttempt/CameraAndDynamicIndexing/PhysicsObject.h
@@ -0,0 +1,30 @@
+#ifndef PHYSICS_OBJECT_H
+#define PHYSICS_OBJECT_H
+
+#include <vector>
+#include <DirectXMath.h>
+
+class PhysicsObject
+{
+public:
+    PhysicsObject(DirectX::XMFLOAT3 position, DirectX::XMFLOAT3 velocity, DirectX::XMFLOAT3 force, float mass, float dt);
+    PhysicsObject(const PhysicsObject& rhs) = delete;
+    PhysicsObject& operator=(const PhysicsObject rhs) = delete;
+    ~PhysicsObject();
+
+    const DirectX::XMFLOAT3& Position()const { return position; }
+    const DirectX::XMFLOAT3& Velocity()const { return velocity; }
+    const DirectX::XMFLOAT3& Force()const { return force; }
+    const float Mass()const { return mass; }
+
+    void Update(float dt);
+
+private:
+    DirectX::XMFLOAT3 position;
+    DirectX::XMFLOAT3 velocity;
+    DirectX::XMFLOAT3 force;
+    float mass;
+    float timeStep = 0.0f;
+};
+
+#endif // PHYSICS_OBJECT_H
